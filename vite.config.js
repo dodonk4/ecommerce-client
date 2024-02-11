4802f5bec72f6d14406871488f2a2de0e08d9fcd
dodonk4 <50113883+dodonk4@users.noreply.github.com>
2024-02-11 11:43:09 -0300

    11/2/2024 deleting port

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/ecommerce-client/",
+  base: "/ecommerce-client/",
   server: {
-    port: 8080,
+    // port: 8080,
     proxy: {
       '/api': 'https://ecommerce-api-black-theta.vercel.app/',
     },
