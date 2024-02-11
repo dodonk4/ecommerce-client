a177afff68ac13b3cf451eac0caf47d153840ab6
dodonk4 <50113883+dodonk4@users.noreply.github.com>
2024-02-11 11:08:28 -0300

    deleting base 11/2/2024

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ecommerce-client/",
+  // base: "/ecommerce-client/",
   server: {
     port: 8080,
     proxy: {
